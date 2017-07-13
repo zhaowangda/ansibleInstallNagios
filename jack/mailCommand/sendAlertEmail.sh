e92bfc36ed15561d20b0a4ca88766a6bbd33d942
zhaowangda <zhaowangda@126.com>
2017-07-13 10:09:53 +0800

    resolve the Chinese word  not ok!

--- jack/mailCommand/sendAlertEmail.sh
+++ jack/mailCommand/sendAlertEmail.sh
@@ -5,7 +5,20 @@ sender=wuxi-metro@chinac.com
 recipient=$1
 smtp=smtp.chinac.com
 cipher=ZUpNT7:FfL6K@]vc19@VW3K2ZsQ]op
-subject=$2
+subject=`iconv -t GB2312 -f UTF-8  << EOF
+$2
+EOF`
+
+#messageInfo=`iconv -t GB2312 -f UTF-8  << EOF
+#$3
+#EOF`
+
+
+
+
+#subject=$2
 messageInfo=$3
 
-$sendEmail -u "$subject" -t $recipient -m "$messageInfo"  -f $sender  -o tls=no  -s $smtp -xu  $sender -xp $cipher
+
+$sendEmail -u "$subject" -t $recipient -m "$messageInfo"  -f $sender  -o tls=no -s $smtp -xu  $sender -xp $cipher  
+#-o message-charset=GB2312 -o message-header=GB2312
